3cf41e409150272d84227bbf7d6b75b756176a6d
dine <dine@20hz.co.uk>
2018-10-08 23:47:51 +0100

    remove scheduler

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#include "event.h"
-#include "testtask.h"
 
 #ifdef IS_BARE_METAL
 extern "C" int mcuBoot(void);
@@ -11,37 +9,12 @@ extern "C" int mcuBoot(void);
 //#include <thread.h>
 #endif
 
-static EventScheduler s("main");
-volatile uint32_t cnt=20000;
-volatile unsigned long tn;
-
-static short sam[2];
-static float ti=0;
-
 
 int main(int argc,char**argv)
 {
 #ifdef IS_BARE_METAL
     mcuBoot();
 #endif
-    {
-        TestTask t(&s,"test 1",2000);
-        t.schedule(s,1);
-    }
-    {
-        TestTask t(&s,"test 2",2000);
-        t.schedule(s,1);
-    }
-    {
-        TestTask t(&s,"test 3",2000);
-        t.schedule(s,1);
-    }
-    while(cnt--){
-//        tn=clock();
-        s.clock();
-#ifdef IS_BARE_METAL
-#else
-//        std::this_thread::sleep_for(std::chrono::milliseconds(1));
-#endif
+    while(1){
     }
 }
