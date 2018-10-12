0bf9a05fa077bcff5b1a2c33f7ebba87027758da
dine <dine@20hz.co.uk>
2018-10-12 02:00:12 +0100

    simplify led heartbeat

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,22 @@
 #include "led.h"
 #include "board.h"
 
+
+__attribute__ ((section(".sram")))
 volatile uint32_t g_systickCounter=0;
 
-
-//#define HEARTBEAT_USE_ALL_LEDS
 extern "C" void SysTick_Handler()
 {
-#ifdef HEARTBEAT_USE_ALL_LEDS
-    for(uint32_t offset=0;offset<ledCount();offset++)
-#else
-#   define offset 0
-#endif
-    {
-        uint32_t tickpos=(((1000/ledCount())*offset)+g_systickCounter)%1000;
-        if(tickpos==0 || tickpos==200){
-            setLed(offset,1);
-        }else if(tickpos==100 || tickpos==300){
-            setLed(offset,0);
-        }
+    uint32_t tickpos=g_systickCounter%1000;
+    if(tickpos==0 || tickpos==200){
+        setLed(0,1);
+    }else if(tickpos==100 || tickpos==300){
+        setLed(0,0);
     }
     g_systickCounter++;
 }
 
 #endif
-//__attribute__ ((section(".textnvm")))
 int main()
 {
 #ifdef IS_BARE_METAL
